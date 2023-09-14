3ca6daace491f29389bc04d0ae09c9e7bf98c5b8
Aleksandr Zaitsev <me@axv.email>
2023-09-14 22:02:42 +0300

    add unsage pointer convert

--- posts/integer-sign/examples/go/main.go
+++ posts/integer-sign/examples/go/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
+
+func s2u(s int8) uint8 {
+	return *(*uint8)(unsafe.Pointer(&s))
+}
 
 func main() {
 	var (
@@ -8,18 +15,19 @@ func main() {
 		unsigned uint8
 	)
 
-	fmt.Println("+---------------------------------------------------------------+")
-	fmt.Println("|              Compare signed and unsigned numbers              |")
-	fmt.Println("+-----+----------+----------+-----+-----------+-----------+-----+")
-	fmt.Println("| #   | unsigned |   bin    | hex |   signed  |    bin    | hex |")
-	fmt.Println("|-----|----------|----------|-----|-----------|-----------|-----|")
+	fmt.Println("+---------------------------------------------------------------------------------+")
+	fmt.Println("|                    Compare signed and unsigned numbers                          |")
+	fmt.Println("+-----+----------+----------+-----+--------+-----------+-----+--------------------+")
+	fmt.Println("| #   | unsigned |   bin    | hex | signed |    bin    | hex | signed as unsigned |")
+	fmt.Println("|-----|----------|----------|-----|--------|-----------|-----|--------------------|")
 
 	for i := 0; i < 260; i++ {
 		signed = int8(i)
 		unsigned = uint8(i)
+		signedAsUnsigned := s2u(signed)
 
-		fmt.Printf("| %-3[1]d | %-8[2]d | %-8[2]b | %-3[2]x | %-9[3]b | %-9[3]b | %-3[3]x |\n", i, unsigned, signed)
+		fmt.Printf("| %-3[1]d | %-8[2]d | %-8[2]b | %-3[2]x | %-6[3]d | %-9[3]b | %-3[3]x | %-18[4]b |\n", i, unsigned, signed, signedAsUnsigned)
 	}
 
-	fmt.Println("+-----+----------+----------+-----+-----------+-----------+-----+")
+	fmt.Println("+-----+----------+----------+-----+--------+-----------+-----+--------------------+")
 }
